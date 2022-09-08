9c84cc282c78513e470460a655c8d27845e9ca33
cklewar@t-online.de <cklewar@t-online.de>
2022-09-08 11:15:37 +0200

    az login

--- versions.tf
+++ versions.tf
@@ -1,15 +1,23 @@
 terraform {
-  required_version = ">= 1.2.5"
+  required_version = ">= 1.2.7"
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "f5-xc-azure-vnet-multinode"
+    }
+  }
 
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = ">= 0.11.11"
+      version = ">= 0.11.12"
     }
 
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 3.17.0"
+      version = ">= 3.21.1"
     }
 
     local = ">= 2.2.3"
